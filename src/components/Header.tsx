f38b73d4288f537db01c4b95e6f000dca117fe39
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-11-07 18:20:28 +0530

    Styling fixes for responsive layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import { IoMoonOutline } from "react-icons/io5";
 function Header() {
   return (
     <header className="header">
-      <h2>Where in the world?</h2>
+      <h2 className="heading-logo">Where in the world?</h2>
       <button className="btn-theme-toggle">
-        <IoMoonOutline />
+        <IoMoonOutline className="moon-icon" />
         Dark Mode
       </button>
     </header>
